31c1d42c4766dcb6f928f4e71461cd41886e7fc6
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2017-03-21 23:00:38 +0800

    归档公共类

---
+++ Archive.m
@@ -0,0 +1,44 @@
+//
+//  Archive.m
+//  Unity
+//
+//  Created by mgfjx on 2017/3/21.
+//  Copyright © 2017年 XXL. All rights reserved.
+//
+
+#import "Archive.h"
+#import <objc/runtime.h>
+
+@implementation ArchiveObj
+
+- (void)encodeWithCoder:(NSCoder *)aCoder{
+    
+    unsigned int outCount;
+    Ivar *ivars = class_copyIvarList([self class], &outCount);
+    for (int i = 0; i < outCount; i++) {
+        Ivar var = ivars[i];
+        const char *name = ivar_getName(var);
+        NSString *key = [NSString stringWithCString:name encoding:NSUTF8StringEncoding];
+        id value = object_getIvar(self, var);
+        [aCoder encodeObject:value forKey:key];
+    }
+    
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super init];
+    if (self) {
+        unsigned int outCount;
+        Ivar *ivars = class_copyIvarList([self class], &outCount);
+        for (int i = 0; i < outCount; i++) {
+            Ivar var = ivars[i];
+            const char *name = ivar_getName(var);
+            NSString *key = [NSString stringWithCString:name encoding:NSUTF8StringEncoding];
+            id value = [aDecoder decodeObjectForKey:key];
+            [self setValue:value forKey:key];
+        }
+    }
+    return self;
+}
+
+@end
